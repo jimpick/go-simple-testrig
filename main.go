369fed5ae45e2dc45ccf0fde558f5a868696b587
Jim Pick <jim@backblend.com>
2019-07-04 20:07:04 -0700

    First attempt at making a server

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	gostream "github.com/libp2p/go-libp2p-gostream"
+
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/host"
+	peerstore "github.com/libp2p/go-libp2p-peerstore"
+	"github.com/libp2p/go-libp2p-http"
+	multiaddr "github.com/multiformats/go-multiaddr"
+)
+
+// newHost illustrates how to build a libp2p host with secio using
+// a randomly generated key-pair
+func newHost(listen multiaddr.Multiaddr) host.Host {
+	h, err := libp2p.New(
+		context.Background(),
+		libp2p.ListenAddrs(listen),
+	)
+	if err != nil {
+		panic(err)
+	}
+	// print the node's listening addresses
+	fmt.Println("Listen addresses:", h.Addrs())
+	return h
+}
+
+func main() {
+	m1, _ := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/10000")
+	srvHost := newHost(m1)
+	// defer srvHost.Close()
+
+	// print the node's PeerInfo in multiaddr format
+	peerInfo := &peerstore.PeerInfo{
+		ID:    srvHost.ID(),
+		Addrs: srvHost.Addrs(),
+	}
+	addrs, err := peerstore.InfoToP2pAddrs(peerInfo)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("libp2p node address:", addrs[0])
+
+	// srvHost.Peerstore().AddAddrs(clientHost.ID(), clientHost.Addrs(), peerstore.PermanentAddrTTL)
+
+	fmt.Println("protocol:", p2phttp.DefaultP2PProtocol)
+	listener, _ := gostream.Listen(srvHost, p2phttp.DefaultP2PProtocol)
+	// defer listener.Close()
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Hi!"))
+		})
+		server := &http.Server{}
+		server.Serve(listener)
+	}()
+
+	// wait for a SIGINT or SIGTERM signal
+        ch := make(chan os.Signal, 1)
+        signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+        <-ch
+        fmt.Println("Received signal, shutting down...")
+
+        // shut the node down
+        if err := listener.Close(); err != nil {
+                panic(err)
+        }
+}
